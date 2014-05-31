ff62ab82c9eff48d4ba00bf5f50a472e097e4a02
gaetan-che <gaetan.chevalley@heig-vd.ch>
2014-05-31 16:02:26 +0200

    Création des types, remplissage des tables des types

--- src/poketournament/PokeTournament.java
+++ src/poketournament/PokeTournament.java
@@ -10,36 +10,67 @@ import java.util.Collections;
 import view.ChooseView;
 
 /**
- *
+ * 
  * @author Romain Therisod
  */
 public class PokeTournament {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Constante.init();
-        
-        Type eau = new Type("eau");
-        ArrayList<Attack> skillsFlorizarre = new ArrayList<>(4);
-        skillsFlorizarre.add(new Attack("Attack1", 100, 50, eau));
-        skillsFlorizarre.add(new Attack("Attack2", 100, 50, eau));
-        skillsFlorizarre.add(new Attack("Attack3", 100, 50, eau));
-        skillsFlorizarre.add(new Attack("Attack4", 100, 50, eau));
-        
-        new Pokemon("Florizarre", eau, 187,152,152,132).setSkillList(skillsFlorizarre); 
-        new Pokemon("Tortank", eau, 186,137,157,130).setSkillList(skillsFlorizarre);
-        new Pokemon("Dracaufeu", eau, 185,161,137,120).setSkillList(skillsFlorizarre);
-        new Pokemon("Ectoplasma", eau, 167,182,127,162).setSkillList(skillsFlorizarre);
-        new Pokemon("Rhinoferoce", eau, 212,182,172,92).setSkillList(skillsFlorizarre);
-        new Pokemon("Voltali", eau, 172,162,147,182).setSkillList(skillsFlorizarre);
-        new Pokemon("Ronflex", eau, 267,162,162,82).setSkillList(skillsFlorizarre);
-        new Pokemon("Dracolosse", eau, 198,186,147,132).setSkillList(skillsFlorizarre);
-        
-        new ChooseView();
-        Collections.shuffle(Pokemon.pokemons);
-        
-    }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		Constante.init();
+
+		Type water = new Type("Eau");
+		Type grass = new Type("Plante");
+		Type fire = new Type("Feu");
+		Type normal = new Type("Normal");
+		Type dragon = new Type("Dragon");
+		Type electric = new Type("Electrique");
+		Type ground = new Type("Sol");
+		Type rock = new Type("Roche");
+		Type fighting = new Type("Combat");
+		Type poison = new Type("Poison");
+		Type ghost = new Type("Spectre");
+		Type ice = new Type("Glace");
+		Type bug = new Type("Insecte");
+		Type psy = new Type("Psy");
+		Type dark = new Type("Ténèbre");
+		Type flying = new Type("Vol");
+		Type steel = new Type("Acier");
+
+		water.setResist(water, steel, fire, ice);
+		water.setWeakness(grass, electric);
+		grass.setResist(water, electric, grass, rock);
+		grass.setWeakness(fire, ice, bug, poison, flying);
+
+		ArrayList<Attack> skillsFlorizarre = new ArrayList<>(4);
+		skillsFlorizarre.add(new Attack("Attack1", 100, 50, water));
+		skillsFlorizarre.add(new Attack("Attack2", 100, 50, water));
+		skillsFlorizarre.add(new Attack("Attack3", 100, 50, water));
+		skillsFlorizarre.add(new Attack("Attack4", 100, 50, water));
+
+		new Pokemon("Florizarre", water, 187, 152, 152, 132)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Tortank", water, 186, 137, 157, 130)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Dracaufeu", water, 185, 161, 137, 120)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Ectoplasma", water, 167, 182, 127, 162)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Rhinoferoce", water, 212, 182, 172, 92)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Voltali", water, 172, 162, 147, 182)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Ronflex", water, 267, 162, 162, 82)
+				.setSkillList(skillsFlorizarre);
+		new Pokemon("Dracolosse", water, 198, 186, 147, 132)
+				.setSkillList(skillsFlorizarre);
+
+		new ChooseView();
+		Collections.shuffle(Pokemon.pokemons);
+
+	}
 }
--- src/poketournament/Type.java
+++ src/poketournament/Type.java
@@ -1,10 +1,11 @@
 package poketournament;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Type {
 	
-	public static LinkedList<Type> types = new LinkedList<Type>();
+	public static HashMap<String, Type> types = new HashMap<String, Type>();
 	
 	private String name;
 	private LinkedList<Type> weakness;
@@ -13,7 +14,7 @@ public class Type {
 	
 	public Type(String name) {
 		this.name = name;
-		types.add(this);
+		types.put(name, this);
 		weakness = new LinkedList<Type> ();
 		resist = new LinkedList<Type> ();
 		immunity = new LinkedList<Type> ();

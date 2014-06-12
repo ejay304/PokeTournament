250770f62ef7f14a6235652580db270785376e98
gaetan-che <gaetan.chevalley@heig-vd.ch>
2014-06-12 15:17:16 +0200

    Correction des problèmes de stab et d'efficacité des attaques. Bad programming is bad.

--- src/mediator/FightMediator.java
+++ src/mediator/FightMediator.java
@@ -53,11 +53,11 @@ public class FightMediator extends Observable implements Mediator{
 
     // c.f. : http://www.pokepedia.fr/index.php/Calcul_des_d%C3%A9g%C3%A2ts
     public void attack(Pokemon source, Attack attack) {
-        double stab = (source.getType() == attack.getType() ? 1.0 : 1.5);
-        double factor = source.getType().getVulnerabilityFactor(attack.getType());
+        double stab = (source.getType() == attack.getType() ? 1.5 : 1.0);
+        double factor = 1;
         if (source == chosenPkmn) {
             System.out.println("/" + source.getName() + " attaque " + pkmnEnnemi.getName() + " avec " + attack.getName());
-            
+            factor = pkmnEnnemi.getType().getVulnerabilityFactor(attack.getType());
             pkmnEnnemiHP -= ((50 * 0.4 + 2) * source.getAttack() * (attack.getPower() * stab)) / (pkmnEnnemi.getDefense() * 50) * factor;
             if (pkmnEnnemiHP <= 0) {
                 System.out.println("PKMN ennemi KO !!");
@@ -68,6 +68,7 @@ public class FightMediator extends Observable implements Mediator{
             }
         } else {
             System.out.println("/" + pkmnEnnemi.getName() + " attaque " + source + " avec " + attack.getName());            
+            factor = chosenPkmn.getType().getVulnerabilityFactor(attack.getType());
             pkmnChoisiHP -= ((50 * 0.4 + 2) * source.getAttack() * (attack.getPower() * stab)) / (chosenPkmn.getDefense() * 50) * factor;
             if (pkmnChoisiHP <= 0) {
                 System.out.println("T'es KO !! ");
@@ -78,6 +79,7 @@ public class FightMediator extends Observable implements Mediator{
             }
 
         }
+        System.out.println("stab:" + stab + "effectiveness:" + factor);
     }
 
     public Pokemon getPokemonCourrant() {

dafc6979ca1f4410b55e84b8674fe095f4910395
Alain Fresco <alain.fresco@gmail.com>
2014-06-17 10:22:55 +0200

    Gestion de l'initialisaion des objets

--- src/poketournament/Attack.java
+++ src/poketournament/Attack.java
@@ -1,125 +1,129 @@
 package poketournament;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Attack {
 
-	public static HashMap<String, Attack> attacks = new HashMap<String, Attack>();
-	private String name;
-	private Integer power;
-	private Integer accuracy;
-	private Type type;
-	private Status status;
+    public static HashMap<String, Attack> attacks = new HashMap<String, Attack>();
+    private String name;
+    private Integer power;
+    private Integer accuracy;
+    private Type type;
+    private Status status;
 
-	public Attack(String name, Integer power, Integer accuracy, Type type, Status status) {
-		this.name = name;
-		this.power = power;
-		this.accuracy = accuracy;
-		this.type = type;
-		this.status = status;
-		attacks.put(name, this);
-	}
-     
-     public static void init(){
-     
-         Attack tempete_florale = new Attack("Tempête florale", 90, 100, Type.getType("Plante"));
-        Attack bomb_beurk = new Attack("Bomb-beurk", 90, 100, Type.getType("Poison"));
-        Attack seisme = new Attack("Séisme", 100, 95, Type.getType("Sol"));
-		Attack force = new Attack("Force", 80, 100, Type.getType("Normal"));
-		
-		Attack surf = new Attack("Surf", 90, 100, Type.getType("Eau"));
-		Attack laser_glace = new Attack("Laser-glace", 90, 100, Type.getType("Glace"));
-		skills.add(new Attack("Eclate-roc", 70, 100, Type.getType("Combat")));
-		skills.add(new Attack("Morsure", 75, 100, Type.getType("Ténèbre")));
-		new Pokemon("Tortank", Type.getType("Eau"), 186, 137, 157, 130)
-				.setSkillList(skills);
-		
-		skills.clear();
-		skills.add(new Attack("Lance-flamme", 90, 100, Type.getType("Feu")));
-		skills.add(new Attack("Cru-aile", 85, 100, Type.getType("Vol")));
-		skills.add(new Attack("Eboulement", 100, 95, Type.getType("Roche")));
-		skills.add(new Attack("Dracogriffe", 85, 100, Type.getType("Dragon")));
-		new Pokemon("Dracaufeu", Type.getType("Feu"), 185, 161, 137, 120)
-				.setSkillList(skills);
-          
-		skills.clear();
-		skills.add(new Attack("Ball'ombre", 90, 100, Type.getType("Spectre")));
-		skills.add(new Attack("Vibrobscur", 85, 100, Type.getType("Ténèbre")));
-		skills.add(new Attack("Psyko", 90, 100, Type.getType("Psy")));
-		skills.add(new Attack("Tonnerre", 90, 100, Type.getType("Electrique")));
-		new Pokemon("Ectoplasma", Type.getType("Spectre"), 167, 182, 127, 162)
-				.setSkillList(skills);
-		
-		skills.clear();
-		skills.add(new Attack("Lame roc", 110, 85, Type.getType("Roche")));
-		skills.add(Attack.getAttack("Séisme"));
-		skills.add(new Attack("Marto-poing", 80, 100, Type.getType("Combat")));
-		skills.add(new Attack("Mégacorne", 110, 85, Type.getType("Insecte")));
-		new Pokemon("Rhinoferos", Type.getType("Roche"), 212, 182, 172, 92)
-				.setSkillList(skills);
-		
-		skills.clear();
-		skills.add(Attack.getAttack("Tonnerre"));
-		skills.add(new Attack("Double-pied", 80, 100, Type.getType("Vol")));
-		skills.add(new Attack("Dard-nuée", 75, 100, Type.getType("Roche")));
-		skills.add(Attack.getAttack("Ball'ombre"));
-		new Pokemon("Voltali", Type.getType("Electrique"), 172, 162, 147, 182)
-				.setSkillList(skills);
-		
-		skills.clear();
-		skills.add(new Attack("Plaquage", 90, 100, Type.getType("Normal")));
-		skills.add(Attack.getAttack("Morsure"));
-		skills.add(new Attack("Roulade", 60, 100, Type.getType("Roche")));
-		skills.add(new Attack("Tacle lourd", 85, 100, Type.getType("Acier")));
-		new Pokemon("Ronflex", Type.getType("Normal"), 267, 162, 162, 82)
-				.setSkillList(skills);
-		
-		skills.clear();
-		skills.add(new Attack("Colère", 110, 85, Type.getType("Dragon")));
-		skills.add(Attack.getAttack("Cru-aile"));
-		skills.add(Attack.getAttack("Surf"));
-		skills.add(Attack.getAttack("Lance-flamme"));
-		new Pokemon("Dracolosse", Type.getType("Dragon"), 198, 186, 147, 132)
-				.setSkillList(skills);
-     
-     }
+    public Attack(String name, Integer power, Integer accuracy, Type type, Status status) {
+        this.name = name;
+        this.power = power;
+        this.accuracy = accuracy;
+        this.type = type;
+        this.status = status;
+        attacks.put(name, this);
+    }
 
-	public String getName() {
-		return name;
-	}
+    public static void init() {
 
-	public Integer getPower() {
-		return power;
-	}
+        ArrayList<Attack> skills = new ArrayList<>(4);
+        skills.add(Attack.getAttack("Tempête florale"));
+        skills.add(Attack.getAttack("Bomb-beurk"));
+        skills.add(Attack.getAttack("Séisme"));
+        skills.add(Attack.getAttack("Force"));
+        new Pokemon("Florizarre", Type.getType("Plante"), 187, 152, 152, 132)
+                .setSkillList(skills);
 
-	public Integer getAccuracy() {
-		return accuracy;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Surf"));
+        skills.add(Attack.getAttack("Laser-glace"));
+        skills.add(Attack.getAttack("Eclate-roc"));
+        skills.add(Attack.getAttack("Morsure"));
+        new Pokemon("Tortank", Type.getType("Eau"), 186, 137, 157, 130)
+                .setSkillList(skills);
 
-	public Type getType() {
-		return type;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Lance-flamme"));
+        skills.add(Attack.getAttack("Cru-aile"));
+        skills.add(Attack.getAttack("Eboulement"));
+        skills.add(Attack.getAttack("Dracogriffe"));
+        new Pokemon("Dracaufeu", Type.getType("Feu"), 185, 161, 137, 120)
+                .setSkillList(skills);
 
-	public static Attack getAttack(String name) {
-		return attacks.get(name);
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Ball'ombre"));
+        skills.add(Attack.getAttack("Vibrobscur"));
+        skills.add(Attack.getAttack("Psyko"));
+        skills.add(Attack.getAttack("Tonnerre"));
+        new Pokemon("Ectoplasma", Type.getType("Spectre"), 167, 182, 127, 162)
+                .setSkillList(skills);
 
-	public boolean doHit() {
-		boolean doHit = true;
-		// Une précision de 100 équivaut à une garantie de toucher
-		if (accuracy < 100) {
+        skills.clear();
+        skills.add(Attack.getAttack("Lame roc"));
+        skills.add(Attack.getAttack("Séisme"));
+        skills.add(Attack.getAttack("Marto-poing"));
+        skills.add(Attack.getAttack("Mégacorne"));
+        new Pokemon("Rhinoferos", Type.getType("Roche"), 212, 182, 172, 92)
+                .setSkillList(skills);
 
-			int hitFactor = RandomNumberGenerator.getInstance()
-					.getRandomNumber(100);
-			System.out.println("hitfactor:" + hitFactor);
-			if (hitFactor > accuracy) {
-				doHit = false;
-			}
-		}
-		return doHit;
-	}
-	
-	public Status getStatus() {
-		return status;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Tonnerre"));
+        skills.add(Attack.getAttack("Double-pied"));
+        skills.add(Attack.getAttack("Dard-nuée"));
+        skills.add(Attack.getAttack("Ball'ombre"));
+        new Pokemon("Voltali", Type.getType("Electrique"), 172, 162, 147, 182)
+                .setSkillList(skills);
+
+        skills.clear();
+        skills.add(Attack.getAttack("Plaquage"));
+        skills.add(Attack.getAttack("Morsure"));
+        skills.add(Attack.getAttack("Roulade"));
+        skills.add(Attack.getAttack("Tacle lourd"));
+        new Pokemon("Ronflex", Type.getType("Normal"), 267, 162, 162, 82)
+                .setSkillList(skills);
+
+        skills.clear();
+        skills.add(Attack.getAttack("Colère"));
+        skills.add(Attack.getAttack("Cru-aile"));
+        skills.add(Attack.getAttack("Surf"));
+        skills.add(Attack.getAttack("Lance-flamme"));
+        new Pokemon("Dracolosse", Type.getType("Dragon"), 198, 186, 147, 132)
+                .setSkillList(skills);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public Integer getAccuracy() {
+        return accuracy;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public static Attack getAttack(String name) {
+        return attacks.get(name);
+    }
+
+    public boolean doHit() {
+        boolean doHit = true;
+        // Une précision de 100 équivaut à une garantie de toucher
+        if (accuracy < 100) {
+
+            int hitFactor = RandomNumberGenerator.getInstance()
+                    .getRandomNumber(100);
+            System.out.println("hitfactor:" + hitFactor);
+            if (hitFactor > accuracy) {
+                doHit = false;
+            }
+        }
+        return doHit;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
 }
--- src/poketournament/Pokemon.java
+++ src/poketournament/Pokemon.java
@@ -5,116 +5,184 @@ import java.util.ArrayList;
 
 /**
  * Pokemon
- * 
+ *
  * @since 27.05.2014
  * @author Alain FRESCO
  * @version 1.0
  */
 public class Pokemon {
 
-	public static ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
+    public static ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
 
-	private Type type;
+    private Type type;
 
-	private String name;
-	private int hp;
-	private int attack;
-	private ArrayList<Attack> skills;
-	private FightMediator mediator;
-	private int defense;
-	private int speed;
-	private Status status;
-	private boolean isIncapacitated;
+    private String name;
+    private int hp;
+    private int attack;
+    private ArrayList<Attack> skills;
+    private FightMediator mediator;
+    private int defense;
+    private int speed;
+    private Status status;
+    private boolean isIncapacitated;
 
-	public Pokemon(String name, Type type, int hp, int attack, int defense,
-			int speed) {
-		this.name = name;
-		this.hp = hp;
-		this.type = type;
-		this.attack = attack;
-		this.defense = defense;
-		this.speed = speed;
-		this.status = Status.EN_FORME;
-		isIncapacitated = false;
-		Pokemon.pokemons.add(this);
-	}
+    public Pokemon(String name, Type type, int hp, int attack, int defense,
+            int speed) {
+        this.name = name;
+        this.hp = hp;
+        this.type = type;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.status = Status.EN_FORME;
+        isIncapacitated = false;
+        Pokemon.pokemons.add(this);
+    }
 
-	public void setMediator(FightMediator fight) {
-		this.mediator = fight;
-	}
+    public static void init() {
 
-	public String getName() {
-		return name;
-	}
+        ArrayList<Attack> skills = new ArrayList<>(4);
+        skills.add(Attack.getAttack("Tempête florale"));
+        skills.add(Attack.getAttack("Bomb-beurk"));
+        skills.add(Attack.getAttack("Séisme"));
+        skills.add(Attack.getAttack("Force"));
+        new Pokemon("Florizarre", Type.getType("Plante"), 187, 152, 152, 132)
+                .setSkillList(skills);
 
-	public void doAttack(Attack attack) {
-		int statusFactor;
+        skills.clear();
+        skills.add(Attack.getAttack("Surf"));
+        skills.add(Attack.getAttack("Laser-glace"));
+        skills.add(Attack.getAttack("Eclate-roc"));
+        skills.add(Attack.getAttack("Morsure"));
+        new Pokemon("Tortank", Type.getType("Eau"), 186, 137, 157, 130)
+                .setSkillList(skills);
 
-		mediator.attack(this, attack);
-		if (attack.getStatus() != null) {
-			statusFactor = RandomNumberGenerator.getInstance().getRandomNumber(
-					3);
-			if (statusFactor == 0) {
-				mediator.changeStatus(this, attack.getStatus());
-			}
+        skills.clear();
+        skills.add(Attack.getAttack("Lance-flamme"));
+        skills.add(Attack.getAttack("Cru-aile"));
+        skills.add(Attack.getAttack("Eboulement"));
+        skills.add(Attack.getAttack("Dracogriffe"));
+        new Pokemon("Dracaufeu", Type.getType("Feu"), 185, 161, 137, 120)
+                .setSkillList(skills);
 
-		}
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Ball'ombre"));
+        skills.add(Attack.getAttack("Vibrobscur"));
+        skills.add(Attack.getAttack("Psyko"));
+        skills.add(Attack.getAttack("Tonnerre"));
+        new Pokemon("Ectoplasma", Type.getType("Spectre"), 167, 182, 127, 162)
+                .setSkillList(skills);
 
-	public int getHp() {
-		return hp;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Lame roc"));
+        skills.add(Attack.getAttack("Séisme"));
+        skills.add(Attack.getAttack("Marto-poing"));
+        skills.add(Attack.getAttack("Mégacorne"));
+        new Pokemon("Rhinoferos", Type.getType("Roche"), 212, 182, 172, 92)
+                .setSkillList(skills);
 
-	public Type getType() {
-		return type;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Tonnerre"));
+        skills.add(Attack.getAttack("Double-pied"));
+        skills.add(Attack.getAttack("Dard-nuée"));
+        skills.add(Attack.getAttack("Ball'ombre"));
+        new Pokemon("Voltali", Type.getType("Electrique"), 172, 162, 147, 182)
+                .setSkillList(skills);
 
-	public ArrayList<Attack> getSkillList() {
-		return skills;
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Plaquage"));
+        skills.add(Attack.getAttack("Morsure"));
+        skills.add(Attack.getAttack("Roulade"));
+        skills.add(Attack.getAttack("Tacle lourd"));
+        new Pokemon("Ronflex", Type.getType("Normal"), 267, 162, 162, 82)
+                .setSkillList(skills);
 
-	public void setSkillList(ArrayList<Attack> skills) {
-		this.skills = (ArrayList<Attack>) skills.clone();
-	}
+        skills.clear();
+        skills.add(Attack.getAttack("Colère"));
+        skills.add(Attack.getAttack("Cru-aile"));
+        skills.add(Attack.getAttack("Surf"));
+        skills.add(Attack.getAttack("Lance-flamme"));
+        new Pokemon("Dracolosse", Type.getType("Dragon"), 198, 186, 147, 132)
+                .setSkillList(skills);
 
-	public int getAttack() {
-		return attack;
-	}
+    }
 
-	public int getDefense() {
-		return defense;
-	}
+    public void setMediator(FightMediator fight) {
+        this.mediator = fight;
+    }
 
-	public int getSpeed() {
-		return speed;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void doStatus() {
-		switch (status) {
+    public void doAttack(Attack attack) {
+        int statusFactor;
 
-		case PARALYSE:
-		case GELE:
-			if (RandomNumberGenerator.getInstance().getRandomNumber(3) == 0) {
-				isIncapacitated = true;
-			}
+        mediator.attack(this, attack);
+        if (attack.getStatus() != null) {
+            statusFactor = RandomNumberGenerator.getInstance().getRandomNumber(
+                    3);
+            if (statusFactor == 0) {
+                mediator.changeStatus(this, attack.getStatus());
+            }
 
-			break;
+        }
+    }
 
-		case POISON:
-		case BRULE:
-			if (RandomNumberGenerator.getInstance().getRandomNumber(3) == 0) {
+    public int getHp() {
+        return hp;
+    }
 
-			}
-			break;
+    public Type getType() {
+        return type;
+    }
 
-		}
-	}
+    public ArrayList<Attack> getSkillList() {
+        return skills;
+    }
 
-	public void setStatus(Status newStatus) {
-		status = newStatus;
-	}
+    public void setSkillList(ArrayList<Attack> skills) {
+        this.skills = (ArrayList<Attack>) skills.clone();
+    }
 
-	private void restoreStatus() {
-		this.status = Status.EN_FORME;
-	}
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void doStatus() {
+        switch (status) {
+
+            case PARALYSE:
+            case GELE:
+                if (RandomNumberGenerator.getInstance().getRandomNumber(3) == 0) {
+                    isIncapacitated = true;
+                }
+
+                break;
+
+            case POISON:
+            case BRULE:
+                if (RandomNumberGenerator.getInstance().getRandomNumber(3) == 0) {
+
+                }
+                break;
+
+        }
+    }
+
+    public void setStatus(Status newStatus) {
+        status = newStatus;
+    }
+
+    private void restoreStatus() {
+        this.status = Status.EN_FORME;
+    }
 }

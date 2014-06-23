f4b8ebff3995a6cc66674a06d99a54fe4f08f6c1
gaetan-che <gaetan.chevalley@heig-vd.ch>
2014-06-23 15:31:34 +0200

    changement de commentaire, je ferais les miens plus tard si y'a des manques

--- src/mediator/FightMediator.java
+++ src/mediator/FightMediator.java
@@ -225,7 +225,7 @@ public class FightMediator extends Observable implements Mediator, Runnable {
 	}
 	
         /**
-         * Attaque sur le temps
+         * Attaque sur le temps (poison, brulure)
          * @param source 
          */
 	public void statusDamage(Pokemon source) {
